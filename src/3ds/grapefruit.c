5f7b91432ebeed336cd9b7d487c07f5eb5e4b7fb
Adrian Siekierka <kontakt@asie.pl>
2020-11-20 18:17:28 +0100

    remove PNG loading for keyboard texture; saves ~60KB

--- src/3ds/grapefruit.c
+++ src/3ds/grapefruit.c
@@ -1,54 +1,14 @@
 #include "grapefruit.h"
-#include <png.h>
+#include <tex3ds.h>
 #include <stdlib.h>
 
-static u32 next_power_of_two(u32 v) {
-	v--;
-	v |= v >> 1;
-	v |= v >> 2;
-	v |= v >> 4;
-	v |= v >> 8;
-	v |= v >> 16;
-	return v + 1;
-}
-
-bool ctr_load_png(C3D_Tex* tex, const char* name, texture_location loc)
+bool ctr_load_t3x(C3D_Tex* tex, const char* name, texture_location loc)
 {
-	png_image img;
-	u32 *data;
-
-	memset(&img, 0, sizeof(png_image));
-	img.version = PNG_IMAGE_VERSION;
-
-	if (!png_image_begin_read_from_file(&img, name)) {
-		return false;
-	}
-
-	img.format = PNG_FORMAT_ABGR;
-
-	if (loc == TEXTURE_TARGET_VRAM) {
-		C3D_TexInitVRAM(tex, next_power_of_two(img.width), next_power_of_two(img.height), GPU_RGBA8);
-	} else {
-		C3D_TexInit(tex, next_power_of_two(img.width), next_power_of_two(img.height), GPU_RGBA8);
-	}
-	data = linearAlloc(tex->width * tex->height * sizeof(u32));
-
-	if (!png_image_finish_read(&img, NULL, data, tex->width * sizeof(u32), NULL)) {
-		linearFree(data);
-		C3D_TexDelete(tex);
-		return false;
-	}
-
-	GSPGPU_FlushDataCache(data, tex->width * tex->height * sizeof(u32));
-
-	C3D_SyncDisplayTransfer(data, GX_BUFFER_DIM(tex->width, tex->height),
-		tex->data, GX_BUFFER_DIM(tex->width, tex->height),
-		GX_TRANSFER_FLIP_VERT(1) | GX_TRANSFER_OUT_TILED(1) | GX_TRANSFER_RAW_COPY(0) |
-		GX_TRANSFER_IN_FORMAT(GPU_RGBA8) | GX_TRANSFER_OUT_FORMAT(GPU_RGBA8)
-		| GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO));
-
-	linearFree(data);
-	png_image_free(&img);
+	FILE *file;
+	file = fopen(name, "rb");
+	if (file == NULL) return false;
+	Tex3DS_TextureImportStdio(file, tex, NULL, loc == TEXTURE_TARGET_VRAM);
+	fclose(file);
 	return true;
 }
 
